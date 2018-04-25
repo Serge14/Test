35c2b0919f40345764e288d46ae369fbb20c28ca
Serge14 <sobukhov@gmail.com>
2018-04-25 18:09:56 +0300

    test commit 2

--- test.R
+++ test.R
@@ -1,3 +1,16 @@
 print("test")
 print("test", quote = FALSE)
 print("test ok", quote = FALSE)
+
+
+ggplot(a, aes(1:nrow(a))) + 
+  geom_line(aes(y = x, colour = "Actual"), size = 1) + 
+  geom_line(aes(y = z, colour = "Predicted"), size = 1) +
+  theme_light() +
+  labs(title = "Sales") +
+  ylab("Sales (kg)") + 
+  xlab("Periods") +
+  theme(axis.title.y = element_text(angle=0, vjust = 1),
+        axis.title.x = element_text(hjust = 1)) +
+  theme(legend.position=c(0.93, 0.85)) +
+  scale_colour_manual(values = c("Actual" = "red", "Predicted" = "blue"))
